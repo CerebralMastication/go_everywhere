d1c276572c3bbcacfe391266ceec5a410ed096a3
JD Long <jal@renre.com>
2019-07-02 09:02:49 -0400

    added a sleep.. set to start at 6080 because of timeout

--- go_everywhere.R
+++ go_everywhere.R
@@ -126,13 +126,15 @@ drive_dist_df <- function(df) {
 }
 
 out_list <- list()
-for (i in 1:nrow(dist_pairs)) {
+for (i in 6080:nrow(dist_pairs)) {
   tic(paste("Started iteration", i))
   out <- drive_dist_df(dist_pairs[i, ])
+  toc()
   out <- cbind(dist_pairs[i, ], out)
   out_list[[i]] <- out
   write_csv(out, "distance_pairs.csv", append = TRUE)
-  toc()
-  Sys.sleep(rlnorm(1000,.1,.5)) #sleep for ~1-4 seconds
+  sleep_time <- rlnorm(1,.4,.75)
+  Sys.sleep(sleep_time) #sleep for ~1-4 seconds
+  print(paste("----------------------------------------------> Sleep time was:", sleep_time))
 }
 
